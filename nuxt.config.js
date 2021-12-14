f74f03bf301cb54a7337dde3ef2e5f6ba6247a70
César S. Liedke <cesarsl@gmail.com>
2021-12-14 17:33:58 -0300

    fix: change build dir

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,36 +1,39 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
+  generate: {
+    dir: "docs",
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title:
-      'César Souza Liedke | Full Stack Web Developer & User Experience Designer',
+      "César Souza Liedke | Full Stack Web Developer & User Experience Designer",
     htmlAttrs: {
-      lang: 'pt-br',
+      lang: "pt-br",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
+        hid: "description",
+        name: "description",
         content:
-          'Opa, tudo certo? Me chamo César Souza Liedke e trabalho como Designer e Desenvolveder de Experiências Digitais. Para falar comigo, é só entrar em contato através dos canais listados aqui no site.',
+          "Opa, tudo certo? Me chamo César Souza Liedke e trabalho como Designer e Desenvolveder de Experiências Digitais. Para falar comigo, é só entrar em contato através dos canais listados aqui no site.",
       },
-      { name: 'format-detection', content: 'telephone=no' },
+      { name: "format-detection", content: "telephone=no" },
     ],
     link: [
-      { rel: 'icon', href: '/favicon.svg' },
+      { rel: "icon", href: "/favicon.svg" },
       {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100..700&display=swap&subset=latin',
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Inter:wght@100..700&display=swap&subset=latin",
       },
     ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['assets/css/main.css'],
+  css: ["assets/css/main.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -41,15 +44,15 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    "@nuxt/typescript-build",
     // https://go.nuxtjs.dev/stylelint
-    '@nuxtjs/stylelint-module',
+    "@nuxtjs/stylelint-module",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/content
-    '@nuxt/content',
+    "@nuxt/content",
   ],
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
@@ -57,4 +60,4 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-}
+};
